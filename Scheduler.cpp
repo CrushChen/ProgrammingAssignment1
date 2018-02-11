0401ac133b9f4fba844b7c0bd873c3f6809bf059
Peter Gish <pgish97@gmail.com>
2018-02-11 14:32:10 -0700

    initial upload

---
+++ Scheduler.cpp
@@ -0,0 +1,61 @@
+/*
+ * File:   Scheduler.cpp
+ * Author: Peter Gish
+ *
+ * Created on February 10, 2018, 5:36 PM
+ */
+
+#include "Scheduler.h"
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::getline;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+Scheduler::Scheduler(std::string file_name_, int block_duration, int time_slice) {
+    BLOCK_DURATION = block_duration;
+    TIME_SLICE = time_slice;
+    global_time = 0;
+    ParseFile(file_name_);
+}
+
+std::vector<Scheduler::Process> Scheduler::ParseFile(std::string file_name_) {
+    vector<Scheduler::Process> processes;
+    /****
+     * TODO:
+     * - Read input file and extract data
+     * - Create std::vector of processes contained all processes that
+     *   need to be run (all process variables having been filled with data 
+     *   from input file
+     *****/
+    return processes;
+}
+
+/****
+ * TODO:
+ * - Implement round robin scheduling algorithm
+ *****/
+void Scheduler::RoundRobin(std::vector<Scheduler::Process> processes) {
+
+}
+
+/****
+ * TODO:
+ * - Implement shortest process next scheduling algorithm
+ *****/
+void Scheduler::ShortestProcessNext(std::vector<Scheduler::Process> processes) {
+
+}
+
+/****
+ * TODO:
+ * - Compute average turn around time of processes
+ *****/
+float Scheduler::AverageTurnaroundTime(std::vector<Scheduler::Process> processes) {
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Peter Gish
+ *
+ * Created on February 10, 2018, 5:23 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * The program should accept three arguments:
+ * 1) input file name
+ * 2) block_duration: the decimal integer time length that a process
+ *                    is unavailable to fun after it blocks
+ * 3) time_slice: the decimal integer of the time slice for the 
+ *                Round-Robin scheduler
+ * -Arguments are passed in the order shown above
+ * 
+ */
+
+#include "Scheduler.h"
+
+#include <cstdlib>
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+    if (argc != 4) {
+        std::cerr << "usage: Assignment1 input_file\n";
+        exit(1);
+    }
+    
+    /* TODO:
+     *  -pass file name and both scheduler parameters to Scheduler class
+     */
+
+    return 0;
+}
+
